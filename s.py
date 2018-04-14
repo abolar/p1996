09c56a5be99420de683a1685bc0c139310a5d04a
unknown <bolarster@gmail.com>
2018-04-14 11:18:16 -0700

    changed to world

--- s.py
+++ s.py
@@ -3,7 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, Universe")
+        self.write("Hello, World!")
 
 def make_app():
     return tornado.web.Application([
